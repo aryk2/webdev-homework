f683f955dc021f2728d2ecf739817abd27ebea10
aryk2 <aryk2@pdx.edu>
2021-11-08 22:24:36 -0500

    excercise 5

--- hw4/05-form.js
+++ hw4/05-form.js
@@ -3,5 +3,16 @@ let form = document.querySelector('form');
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-  // Add your code here
+  event.preventDefault()
+  const name = document.getElementById('name').value
+  const email = document.getElementById('email').value
+  const yes = document.getElementById('yes').value
+  const date = document.getElementById('date').value
+
+  console.log(
+    'name: ', name,
+    '   email: ', email,
+    '   sign me up?', yes,
+    '   date: ', date,
+  )
 }
