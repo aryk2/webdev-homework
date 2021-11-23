671ed28eb32b4e083c7f5c126cf0f77507696f43
aryk2 <aryk2@pdx.edu>
2021-11-22 19:20:20 -0500

    02 done

--- hw5/02-beverages.js
+++ hw5/02-beverages.js
@@ -3,8 +3,36 @@ const url =
 
 let app = document.querySelector('#results');
 
-const fetchData = (url) => {
-  // Add your code here
+const fetchData = async (url) => {
+  try {
+    const result = await (await fetch(url)).json()
+
+    var el = document.getElementById('loading');
+    el.remove();
+
+    result.drinks.forEach(drink => {
+      const image = document.createElement("img");
+      image.setAttribute('id', 'image');
+      image.setAttribute('src', drink?.strDrinkThumb??"");
+      image.setAttribute('alt', 'drink-img');
+
+      const title = document.createElement("p");
+      title.setAttribute('id', 'drink-title');
+      const node = document.createTextNode(drink?.strDrink??"");
+      title.appendChild(node);
+
+      const item = document.createElement("div");
+      item.setAttribute('id', 'item');
+
+      item.appendChild(image);
+      item.appendChild(title);
+
+      app.appendChild(item)
+    })
+
+  } catch(e) {
+    console.error(e)
+  }
 };
 
 fetchData(url);
